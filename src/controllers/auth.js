7d190444517c4292ca9003c37bdefe41bc67368c
Aswin Gangadharan <aswin.gangadharan.2020@gmail.com>
2024-03-04 10:41:18 +0530

    Save user to db and send token response

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,7 +1,52 @@
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+import db from '../utils/prisma.js'
+
+const signToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: '8h',
+  })
+}
+
 export const register = async (req, res) => {
-  res.send('Register user')
+  const { name, email, password } = req.body
+  const hash = bcrypt.hashSync(password)
+
+  const user = await db.user.create({
+    data: {
+      name,
+      email,
+      password: hash,
+    },
+  })
+
+  const token = signToken(user.id)
+
+  res.status(201).json({ token })
 }
 
 export const login = async (req, res) => {
-  res.send('Login user')
+  const { email, password } = req.body
+
+  const user = await db.user.findUnique({
+    where: {
+      email,
+    },
+  })
+
+  if (!user) {
+    return res.status(401).json({
+      message: 'Invalid email or password',
+    })
+  }
+
+  if (!bcrypt.compareSync(password, user.password)) {
+    return res.status(401).json({
+      message: 'Invalid email or password',
+    })
+  }
+
+  const token = signToken(user.id)
+
+  res.status(200).json({ token })
 }
